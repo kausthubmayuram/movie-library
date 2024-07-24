4c548292ad450712e934a6e2b20c61658eb12a0e
Kausthub Mayuram <mayuram.kausthub@nuvama.com>
2024-07-24 16:08:05 +0530

    Added View Movie Details

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  styleUrls: ['./movie-details.component.css'],
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
 
+  movieDetails:any;
+  constructor(
+    private dataService:DataService,
+    private activatedRoute:ActivatedRoute,
+    private router: Router,
+  ) {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      this.getMovieById(id);
+  });
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  getMovieById(id:any){
+    this.dataService.getbyId(id).subscribe((result:any)=>{
+      this.movieDetails = result;
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/manage']);
+  }
+  
 }
